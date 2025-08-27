22aaf52d3d1b038c591b600ef707aad5598bf309
Anton Statut <antonstatut@gmail.com>
2025-08-27 10:31:52 +0300

    Задание выполнено

---
+++ public/loginPage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = function(data) { 
+    ApiConnector.login(data , response => {
+        if (response.error) {
+           console.error('Ошибка входа:', response.error);
+           this.setLoginErrorMessage(response.error);
+        } else {
+           location.reload();
+        };
+    });
+};
+
+userForm.registerFormCallback = function(data) {
+    ApiConnector.register(data, response => {
+        if (response.error) {
+           console.error('Ошибка регистрации:', response.error);
+           this.setRegisterErrorMessage(response.error);
+        } else {
+           location.reload();
+        };
+    });
+};
